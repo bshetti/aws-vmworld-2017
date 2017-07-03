358c6a0e9c0614a9074ee238f5be9de720fbbae8
Ubuntu <ubuntu@ip-172-31-5-204.us-west-2.compute.internal>
2017-07-03 00:51:04 +0000

    first commit

---
+++ testvpc.tf
@@ -0,0 +1,224 @@
+variable "VPCNAME" {}
+
+variable "akey" {}
+variable "skey" {}
+variable "awsregion" {}
+
+provider "aws" {
+  access_key = "${var.akey}"
+  secret_key = "${var.skey}"
+  region     = "${var.awsregion}"
+}
+
+variable "images" {
+  
+  type = "map"
+
+  default = {
+    web="ami-a7b886b1"
+    mngt="ami-dfb886c9"
+    db="ami-1dbb850b"
+    dblb="ami-71bd8367"
+    app="ami-75bd8363"
+  }
+}
+
+resource "aws_vpc" "vpc_tuto" {
+  cidr_block = "172.31.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.VPCNAME}"
+  }
+}
+
+resource "aws_subnet" "public_subnet_us_west_1a" {
+  vpc_id                  = "${aws_vpc.vpc_tuto.id}"
+  cidr_block              = "172.31.0.0/20"
+  map_public_ip_on_launch = true
+  tags = {
+    Name =  "Subnet main"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.vpc_tuto.id}"
+  tags {
+        Name = "InternetGateway"
+    }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.vpc_tuto.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
+
+resource "aws_route_table_association" "public_subnet_us_west_1a_association" {
+    subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+    route_table_id = "${aws_vpc.vpc_tuto.main_route_table_id}"
+}
+
+resource "aws_security_group" "app_sec_group" {
+    name = "app_sec_group"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 8000
+        to_port     = 8000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    vpc_id = "${aws_vpc.vpc_tuto.id}"
+    
+}
+
+
+resource "aws_instance" "Db1" {
+  ami           = "${var.images["db"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.10.220"
+  tags {
+        Name = "Db1"
+        Tier = "DB"
+  }
+
+}
+
+
+resource "aws_instance" "DbLb" {
+  ami           = "${var.images["dblb"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.14.166"
+  tags {
+        Name = "Db Load Balancer"
+        Tier = "DB"
+  }
+
+}
+
+
+
+resource "aws_instance" "App1" {
+  ami           = "${var.images["app"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.4.252"
+  tags {
+        Name = "App1"
+        Tier = "App"
+  }
+
+}
+
+resource "aws_instance" "App2" {
+  ami           = "${var.images["app"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.13.93"
+  tags {
+        Name = "App2"
+        Tier = "App"
+  }
+
+}
+
+resource "aws_instance" "App3" {
+  ami           = "${var.images["app"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.9.174"
+  tags {
+        Name = "App3"
+        Tier = "App"
+  }
+
+}
+
+resource "aws_instance" "Web1" {
+  ami           = "${var.images["web"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.1.86"
+  tags {
+        Name = "Web1"
+        Tier = "Web"
+  }
+
+}
+
+resource "aws_instance" "Web2" {
+  ami           = "${var.images["web"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.3.177"
+  tags {
+        Name = "Web2"
+        Tier = "Web"
+  }
+
+}
+
+resource "aws_instance" "Db2" {
+  ami           = "${var.images["db"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.10.117"
+  tags {
+        Name = "Db-2"
+        Tier = "DB"
+  }
+
+}
+
+resource "aws_instance" "Mngt" {
+  ami           = "${var.images["mngt"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  private_ip                  = "172.31.11.68"
+  tags {
+        Name = "Management"
+        Tier = "Mngt"
+  }
+
+}
